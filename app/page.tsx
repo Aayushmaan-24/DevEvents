f1c150fa4f2c24c0d1874c8d2351db9668191b8d
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-01-09 20:15:31 +0530

    added events to homepage using placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import EventCard from '@/components/EventCard'
+import ExploreBtn from '@/components/ExploreBtn'
+import events from '@/lib/constants'
+
 
 const Page = () => {
   return (
     <section>
       <h1 className='text-center'>Hub for Every Tech Event <br /> You cant Miss </h1>
       <p className='text-center mt-5'> Hackathons, meetups and conferences, All at one place</p>
+      <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+  
     </section>
   )
 }
